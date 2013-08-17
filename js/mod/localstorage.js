97837aec66a55e4cf57f7db9b6f0b68b15b4a2c5
LestatSeven <lestat.sevendays@gmail.com>
2013-08-17 23:39:16 +0800

    complete

--- js/mod/localstorage.js
+++ js/mod/localstorage.js
@@ -1,35 +1,35 @@
-	var Local = {
-		state: [[],[]],
-		get: function (key) {
-			var i = this.state[0].indexOf(key);
-			if(i == -1) {
-				this.state[0].push(key);
-				this.state[1].push(false);
-				i = this.state[0].length-1;
-			}
+var Local = {
+	state: [[],[]],
+	get: function (key) {
+		var i = this.state[0].indexOf(key);
+		if(i == -1) {
+			this.state[0].push(key);
+			this.state[1].push(false);
+			i = this.state[0].length-1;
+		}
 
-			try {
-				var ls = localStorage[key];
-				if([undefined, ''].indexOf(ls.trim()) == -1) {
-					this.state[1][i] = true;
-					return ls;
-				}
-			} catch(e) {
-				return '';
-			}
-		},
-		set: function (key, val) {
-			var i = this.state[0].indexOf(key);
-			if(val.trim() !== '') {
+		try {
+			var ls = localStorage[key];
+			if([undefined, ''].indexOf(ls.trim()) == -1) {
 				this.state[1][i] = true;
-				try {
-					localStorage.setItem(key, val);
-				} catch(e) {};
-			} else {
-				this.state[1][i] = false;
-				try {
-					localStorage.removeItem(key);
-				} catch(e) {};
+				return ls;
 			}
+		} catch(e) {
+			return '';
+		}
+	},
+	set: function (key, val) {
+		var i = this.state[0].indexOf(key);
+		if(val.trim() !== '') {
+			this.state[1][i] = true;
+			try {
+				localStorage.setItem(key, val);
+			} catch(e) {};
+		} else {
+			this.state[1][i] = false;
+			try {
+				localStorage.removeItem(key);
+			} catch(e) {};
 		}
 	}
+}
